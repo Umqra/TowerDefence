113dd61923b1b110dbd4b9d1a8536b1e27769ad8
Umqra <golovoiapklavu@gmail.com>
2015-10-22 13:48:30 +0500

    Menu toolbar

--- Gui/gui_tower_defence.py
+++ Gui/gui_tower_defence.py
@@ -1,5 +1,6 @@
 import jsonpickle
 import pickle
+from PyQt4.QtCore import Qt
 from Controller.creator_controller import CreatorController
 from Controller.main_controller import MainController
 from Gui import start_gui
@@ -7,13 +8,21 @@ from Infrastructure.pyqt_helpers import clear_layout
 from Model.level_loader import Level1, Level2, load_level_from_file, get_level_loader, levels
 from Model.time import Time
 from Model.warriors import BFSWalker
+from PyQtExtension.scrollable_messagebox import ScrollableMessageBox
 from View.creator_view import CreatorView
 
 __author__ = 'umqra'
-from PyQt4.QtGui import QGridLayout, QMenuBar, QVBoxLayout, QMenu, QShortcut, QKeySequence
+from PyQt4.QtGui import QGridLayout, QMenuBar, QVBoxLayout, QMenu, QShortcut, QKeySequence, QMessageBox
 from Model.game_state import GameState
 from View.state_view import StateView
 from PyQt4 import QtGui, QtCore
+
+markdown_exist = False
+try:
+    import markdown
+    markdown_exist = True
+except ImportError:
+    markdown_exist = False
 
 
 class Game(QtGui.QWidget):
@@ -23,7 +32,7 @@ class Game(QtGui.QWidget):
     def __init__(self):
         super().__init__()
         self.setFixedSize(1000, 800)
-        self.layout = QVBoxLayout()
+        self.layout = QGridLayout()
         self.setLayout(self.layout)
         self.state = None
         self.state_view = None
@@ -36,20 +45,43 @@ class Game(QtGui.QWidget):
         self.timer = QtCore.QBasicTimer()
         self.timer.start(Game.interval, self)
 
-        menu_bar = QMenuBar(self)
-        game_menu = menu_bar.addMenu("Game")
-        game_menu.addAction('Create level', self.load_level_creator,
-                            QKeySequence("Ctrl+N"))
-        game_menu.addAction('Start game', lambda: self.load_level(self.last_level),
-                            QKeySequence("Ctrl+R"))
-        game_menu.addAction('Pause game', lambda: self.state.stop(), QKeySequence("Ctrl+P"))
-        game_menu.addAction('Run game', lambda: self.state.resume(), QKeySequence("Ctrl+O"))
+        self.menu_bar = QMenuBar(self)
+        self.menu_bar.setMinimumWidth(self.width())
+        game_menu = self.menu_bar.addMenu("Game")
+        help_menu = self.menu_bar.addMenu("Help")
+        help_menu.addAction("Help topics", self.load_help)
+        help_menu.addAction("About", self.show_about)
+
+        game_menu.addAction('Create level', self.load_level_creator)
+        game_menu.addAction('Start game', lambda: self.load_level(self.last_level))
+        game_menu.addAction('Pause game', lambda: self.state.stop())
+        game_menu.addAction('Run game', lambda: self.state.resume())
+        game_menu.addAction('Exit', lambda: self.close())
+
+        self.layout.setRowMinimumHeight(0, 20)
+
+    def show_about(self):
+        about = QMessageBox.about(self, 'About game', 'Tower defence\nUrFU 2015')
+
+    def load_help(self):
+        help_info = 'Help info unavailable'
+        with open('README.md') as f:
+            help_info = f.read()
+            if markdown_exist:
+                help_info = markdown.markdown(help_info)
+            else:
+                help_info = 'Markdown unavailable.\nYou can install it with command: pip install markdown\n' + help_info
+        x = ScrollableMessageBox(self)
+        x.setText(help_info)
+        x.setWindowTitle('2')
+        x.show()
+        # help = QMessageBox.about(self, 'Help', help_info)
 
     def load_level_from_file(self, file_name):
         self.reset_game()
         self.state = load_level_from_file(file_name)
         self.state_view = StateView(self.state)
-        self.layout.addWidget(self.state_view)
+        self.layout.addWidget(self.state_view, 1, 0)
 
         self.setMouseTracking(True)
 
@@ -59,7 +91,7 @@ class Game(QtGui.QWidget):
         self.state = GameState(self)
         self.state.initialize_with_loader(level_loader)
         self.state_view = StateView(self.state)
-        self.layout.addWidget(self.state_view)
+        self.layout.addWidget(self.state_view, 1, 0)
 
         self.setMouseTracking(True)
 
@@ -69,7 +101,7 @@ class Game(QtGui.QWidget):
         self.state.initialize_empty_level()
         self.state.set_controller(CreatorController(self.state))
         self.state_view = CreatorView(self.state)
-        self.layout.addWidget(self.state_view)
+        self.layout.addWidget(self.state_view, 1, 0)
 
         self.setMouseTracking(True)
 
---
+++ PyQtExtension/scrollable_messagebox.py
@@ -0,0 +1,20 @@
+from PyQt4.QtGui import QMessageBox, QLabel, QScrollArea, QVBoxLayout
+from PyQt4.QtCore import Qt
+
+__author__ = 'umqra'
+
+
+class ScrollableMessageBox(QMessageBox):
+    def __init__(self, *__args):
+        super().__init__(*__args)
+        self.container = QLabel()
+        self.scroller = QScrollArea()
+        self.scroller.setMinimumHeight(400)
+        self.scroller.setMinimumWidth(500)
+        self.scroller.setWidgetResizable(True)
+        self.scroller.setWidget(self.container)
+        self.layout().addWidget(self.scroller, 0, 0)
+        self.layout().setColumnStretch(1, 1)
+
+    def setText(self, p_str):
+        self.container.setText(p_str)
